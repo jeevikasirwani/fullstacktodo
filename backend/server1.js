06f6907ca5e017f4102f44aed08d12bf83664aaf
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2024-08-01 20:09:37 +0530

    2  endpoint

--- backend/server1.js
+++ backend/server1.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const PORT=3000;
-import {createTodo} from "./types";
+import {createTodo, updateTodo} from "./types";
 //title description
  
 app.get("/todo",(req,res)=>{
@@ -23,7 +23,14 @@ app.get("/todos",(req,res)=>{
 
 
 app.put("/completed",(req,res)=>{
-   
+    const updatePayload=req.body;
+    const validate=updateTodo.safeParse(updatePayload);
+    if(validate.success){
+        res.json({msg:"todo updated"})
+    }
+    else{
+        res.status(403).send({msg:"Tree clean nothing to update"})
+    }
 })
 
 app.listen(PORT);
