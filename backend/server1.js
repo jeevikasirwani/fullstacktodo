d66f167eeb55a3086235280714710f180c5cbc9d
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2024-08-01 20:43:41 +0530

    some updates

--- backend/server1.js
+++ backend/server1.js
@@ -1,42 +1,54 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const PORT=3000;
+const PORT = 3000;
+import mongoose from "mongoose";
 import { TodoModel } from "./db";
-import {createTodo, updateTodo} from "./types";
+import { createTodo, updateTodo } from "./types";
 //title description
- 
-app.get("/todo",async(req,res)=>{
-    const createPayload=req.body;
-    const validate=createTodo.safeParse(createPayload);
-    if(!validate.success){
-        res.status(403).send({msg:"Not a Valid User"})
+
+app.get("/todo", async (req, res) => {
+    const createPayload = req.body;
+    const validate = createTodo.safeParse(createPayload);
+    if (!validate.success) {
+        res.status(403).send({ msg: "Not a Valid User" })
         return;
     }
-  await TodoModel.create({
-    title:createPayload.title,
-    description:createPayload.description
-   })
-   res.json({
-    msg:"User Created"
-   })
+    await TodoModel.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+    res.json({
+        msg: "User Created"
+    })
 })
 
 
-app.get("/todos",(req,res)=>{
-    
+app.get("/todos", async (req, res) => {
+    const todo = await TodoModel.find({});
+    res.json({
+        todo
+    })
 })
 
 
-app.put("/completed",(req,res)=>{
-    const updatePayload=req.body;
-    const validate=updateTodo.safeParse(updatePayload);
-    if(validate.success){
-        res.json({msg:"todo updated"})
+app.put("/completed", async (req, res) => {
+    const updatePayload = req.body;
+    const validate = updateTodo.safeParse(updatePayload);
+    if (!validate.success) {
+        res.status(403).send({ msg: "Tree clean nothing to update" })
+        return;
     }
-    else{
-        res.status(403).send({msg:"Tree clean nothing to update"})
-    }
+    await TodoModel.update({
+        _id: req.body.id
+    }, {
+        completed: true
+    })
+    res.json({
+        msg: "Marked as Completed"
+    })
+
 })
 
 app.listen(PORT);
