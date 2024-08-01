8e565fe6894c41ead5d66f700c24198480dc2b71
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2024-08-01 20:06:46 +0530

    1 endpoint done

--- backend/server1.js
+++ backend/server1.js
@@ -2,11 +2,18 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const PORT=3000;
-
+import {createTodo} from "./types";
 //title description
  
 app.get("/todo",(req,res)=>{
-
+    const createPayload=req.body;
+    const validate=createTodo.safeParse(createPayload);
+    if(validate.success){
+        res.json({msg:"todo added"})
+    }
+    else{
+        res.status(403).send({msg:"Not a Valid User"})
+    }
 })
 
 
@@ -16,7 +23,7 @@ app.get("/todos",(req,res)=>{
 
 
 app.put("/completed",(req,res)=>{
-    
+   
 })
 
 app.listen(PORT);
