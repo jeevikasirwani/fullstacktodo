fbddd5082967bcda6a5bae02abe75c553bbc4b91
Jeevika Sirwani <jeevika.sirwani4@gmail.com>
2024-08-02 14:57:25 +0530

    updates

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,10 @@
+export function CreateTodo(){
+    return <>
+    <input style={{padding:10,margin:10,color:"#747bff",backgroundColor:"#242424",borderRadius:"5px"}} type="text" placeholder="title"/>
+    <input style={{padding:10,margin:10,color:"#747bff",backgroundColor:"#242424",borderRadius:"5px"}} type="text" placeholder="description" />
+
+    <button style={{padding:10,margin:10,backgroundColor:"#242424",color:"#747bff",cursor:"pointer",borderRadius:"5px"}}>Add a Todo</button>
+    </>
+}
+
+
